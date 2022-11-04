24a378353c5bd5e2f98e17ae959aa95de7b38588
Tim Swast <swast@google.com>
2022-11-04 08:56:45 -0500

    refactor stockdice.py into functions

--- stockdice.py
+++ stockdice.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import pathlib
 
 import numpy
@@ -24,49 +25,57 @@ import helpers
 
 DIR = pathlib.Path(__file__).parent
 FMP_DIR = DIR / "third_party" / "financialmodelingprep.com"
-
-
 quote_path = FMP_DIR / "quote.csv"
-quote = pandas.read_csv(quote_path, header=None, names=["symbol", "market_cap"])
 income_path = FMP_DIR / "income-statement.csv"
-income = pandas.read_csv(
-    income_path, header=None, names=["symbol", "profit", "revenue", "currency"],
-)
-income["usd_revenue"] = income.apply(
-    lambda row: helpers.to_usd(row["currency"], row["revenue"]),
-    axis=1,
-)
-income["usd_profit"] = income.apply(
-    lambda row: helpers.to_usd(row["currency"], row["profit"]),
-    axis=1,
-)
-
 balance_sheet_path = FMP_DIR / "balance-sheet-statement.csv"
-balance_sheet = pandas.read_csv(
-    balance_sheet_path, header=None, names=["symbol", "book", "currency"],
-)
-balance_sheet["usd_book"] = balance_sheet.apply(
-    lambda row: helpers.to_usd(row["currency"], row["book"]),
-    axis=1,
-)
 
-screen = quote.merge(
-    income.merge(balance_sheet, how="outer", on="symbol",), how="outer", on="symbol",
-).fillna(value=0)
-screen.drop_duplicates(keep="last")
-screen_ones = numpy.ones(len(screen.index))
 
-# Even weight seemed to skew too heavily towards value. Place a more weight in
-# market cap, since market risk is the main factor I want to target.
-screen["average"] = numpy.exp(
-    (1.0 / 10.0)
-    * (
-        1 * numpy.log(numpy.fmax(screen_ones, screen["usd_book"]))
-        + 2 * numpy.log(numpy.fmax(screen_ones, screen["usd_profit"]))
-        + 2 * numpy.log(numpy.fmax(screen_ones, screen["usd_revenue"]))
-        + 5 * numpy.log(numpy.fmax(screen_ones, screen["market_cap"]))
+def load_dfs():
+    quote = pandas.read_csv(quote_path, header=None, names=["symbol", "market_cap"])
+    income = pandas.read_csv(
+        income_path, header=None, names=["symbol", "profit", "revenue", "currency"],
     )
-)
+    balance_sheet = pandas.read_csv(
+        balance_sheet_path, header=None, names=["symbol", "book", "currency"],
+    )
+    return quote, income, balance_sheet
 
-symbol = screen.sample(weights=screen["average"])
-print(symbol)
+
+def add_usd_column_from_forex(df, column):
+    df[f"usd_{column}"] = df.apply(
+        lambda row: helpers.to_usd(row["currency"], row[column]),
+        axis=1,
+    )
+
+
+
+def main():
+    quote, income, balance_sheet = load_dfs()
+    add_usd_column_from_forex(income, "revenue")
+    add_usd_column_from_forex(income, "profit")
+    add_usd_column_from_forex(balance_sheet, "book")
+
+    screen = quote.merge(
+        income.merge(balance_sheet, how="outer", on="symbol",), how="outer", on="symbol",
+    ).fillna(value=0)
+    screen.drop_duplicates(keep="last")
+    screen_ones = numpy.ones(len(screen.index))
+
+    # Even weight seemed to skew too heavily towards value. Place a more weight in
+    # market cap, since market risk is the main factor I want to target.
+    screen["average"] = numpy.exp(
+        (1.0 / 10.0)
+        * (
+            1 * numpy.log(numpy.fmax(screen_ones, screen["usd_book"]))
+            + 2 * numpy.log(numpy.fmax(screen_ones, screen["usd_profit"]))
+            + 2 * numpy.log(numpy.fmax(screen_ones, screen["usd_revenue"]))
+            + 5 * numpy.log(numpy.fmax(screen_ones, screen["market_cap"]))
+        )
+    )
+
+    symbol = screen.sample(weights=screen["average"])
+    print(symbol)
+
+
+if __name__ == "__main__":
+    main()
